43e643d9213e09acc566fa51dbbb3c7bea343e43
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-06-02 18:43:25 -0700

    added sixth question and top ten

--- game.js
+++ game.js
@@ -3,9 +3,11 @@ console.log('User input for userName: ' + userName);
 alert('Hello, ' + userName + '! Here\'s a page I made about me');
 
 var guess = prompt('Do I own a cat?');
+var counter = 0;
 console.log('User input for guess: ' + guess);
 if (guess.toUpperCase() === 'YES' || guess.toUpperCase() === 'Y') {
   alert('That\'s right, ' + userName + '! But not just one!');
+  counter++;
 } else {
   alert('Incorrect,' + userName + '!');
 }
@@ -16,12 +18,14 @@ if (gender.toUpperCase() === 'YES' || gender.toUpperCase() === 'Y') {
   alert('Sorry, ' + userName + '! I am female, not male');
 } else {
   alert('That\'s right ' + userName + '!');
+  counter++;
 }
 
 var baking = prompt('Do I enjoy baking?');
 console.log('User input for baking: ' + baking);
 if (baking.toUpperCase() === 'YES' || baking.toUpperCase() === 'Y') {
   alert('Correct, ' + userName + '! I\'ll bake you something!');
+  counter++;
 } else {
   alert('That\'s incorrect, ' + userName + '!');
 }
@@ -30,6 +34,7 @@ var eyeColor = prompt('Do I have brown eyes?');
 console.log('User input for eyeColor: ' + eyeColor);
 if (eyeColor.toUpperCase() === 'YES' || eyeColor.toUpperCase() === 'Y') {
   alert('You know me so well ' + userName + '! A little too well...');
+  counter++;
 } else {
   alert('Wrong, ' + userName + '. You don\'t know me at all! But lets get to know each other!');
 }
@@ -40,4 +45,29 @@ if (talent.toUpperCase() === 'YES' || talent.toUpperCase() === 'Y') {
   alert('I wish I was that cool ' + userName + '...');
 } else {
   alert ('That is right, ' + userName + '! But it\'d be cool if I could!');
+  counter++;
 }
+
+var catsTotal = prompt ('How many cats do I have?'); //sixth question
+var guesses = 4;
+var guessLeft = 'You have ' + guesses + ' chances left';
+console.log('User input for catsTotal' + catsTotal);
+
+while (guesses > 0) {
+  if (parseInt(catsTotal) === 5) {
+    alert ('That is correct!');
+    counter++;
+    break;
+  } else if (parseInt(catsTotal) > 5) {
+    guesses--;
+    catsTotal = prompt('That is too many cats ' + guessLeft);
+  } else if (parseInt(catsTotal) < 5) {
+    guesses--;
+    catsTotal = prompt('That is too few cats! ' + guessLeft);
+  } else {
+    alert ('You didn\'t guess correctly! I have 5 cats total.');
+    break;
+  }
+}
+
+alert('You got ' + counter + ' out of 7 questions right!');
